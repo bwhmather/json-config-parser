d6cac97e41cbba0f5f03e775222a86a08ae4ffc5
Ben Mather <bwhmather@gmail.com>
2013-02-11 16:21:59 +0000

    Initial commit

---
+++ jsonconfigparser/__init__.py
@@ -0,0 +1,388 @@
+from collections.abc import MutableMapping
+from collections import OrderedDict
+
+import itertools
+
+import re
+import json
+
+__all__ = ["DuplicateSectionError", "DuplicateOptionError",
+           "JSONConfigParser"]
+
+DEFAULT_SECT = "DEFAULT"
+_UNSET = object()
+
+
+class ParseError(BaseException):
+    pass
+
+
+class MissingSectionHeaderError(BaseException):
+    """Raised if an option occurs before the first header
+    """
+    def __init__(self, filename, lineno, line):
+        BaseException.__init__(
+            self,
+            'No section header before first option.\n' +
+            'file: %s, line: %d\n%r' %
+            (filename, lineno, line))
+
+
+class InvalidSectionNameError(BaseException):
+    pass
+
+
+class InvalidOptionNameError(BaseException):
+    pass
+
+
+class NoSectionError(BaseException):
+    pass
+
+
+class NoOptionError(BaseException):
+    pass
+
+
+class DuplicateSectionError(BaseException):
+    """Raised when a section is repeated in an input source.
+
+    Possible repetitions that raise this exception are: multiple creation
+    using the API or when a section is found more than once in a single input
+    file, string or dictionary.
+    """
+
+    def __init__(self, section, source=None, lineno=None):
+        msg = [repr(section), " already exists"]
+        if source is not None:
+            message = ["While reading from ", source]
+            if lineno is not None:
+                message.append(" [line {0:2d}]".format(lineno))
+            message.append(": section ")
+            message.extend(msg)
+            msg = message
+        else:
+            msg.insert(0, "Section ")
+        BaseException.__init__(self, "".join(msg))
+        self.section = section
+        self.source = source
+        self.lineno = lineno
+        self.args = (section, source, lineno)
+
+
+class DuplicateOptionError(BaseException):
+    """Raised by strict parsers when an option is repeated in an input source.
+
+    Current implementation raises this exception only when an option is found
+    more than once in a single file, string or dictionary.
+    """
+
+    def __init__(self, section, option, source=None, lineno=None):
+        msg = [repr(option), " in section ", repr(section),
+               " already exists"]
+        if source is not None:
+            message = ["While reading from ", source]
+            if lineno is not None:
+                message.append(" [line {0:2d}]".format(lineno))
+            message.append(": option ")
+            message.extend(msg)
+            msg = message
+        else:
+            msg.insert(0, "Option ")
+        BaseException.__init__(self, "".join(msg))
+        self.section = section
+        self.option = option
+        self.source = source
+        self.lineno = lineno
+        self.args = (section, option, source, lineno)
+
+
+class JSONConfigParser(OrderedDict):
+
+    _BLANK_TMPL = r"""
+        ^
+        (\#.*)?                      # optional comment
+        $
+        """
+
+    _SECTION_TMPL = r"""
+        [\-\w]+
+        """
+
+    _HEADER_TMPL = r"""
+        #^
+        \[
+        (?P<section>""" + _SECTION_TMPL + """)
+        \]
+        #$
+        """
+
+    _KEY_TMPL = r"""
+        (?P<key>[\-\w]+)            # at least one letter underscore or hyphen
+        \s*                         # optional whitespace
+        =                           # followed by '='
+        \s*                         # optional whitespace
+        """
+
+    _EOL_TMPL = r"""
+        \s*                         # optional whitespace
+        (\#.*)?                      # optional comment
+        $
+        """
+
+    _blank_re = re.compile(_BLANK_TMPL, re.VERBOSE)
+    _section_re = re.compile('^' + _SECTION_TMPL + '$', re.VERBOSE)
+    _header_re = re.compile(_HEADER_TMPL, re.VERBOSE)
+    _key_re = re.compile(_KEY_TMPL, re.VERBOSE)
+    _eol_re = re.compile(_EOL_TMPL, re.VERBOSE)
+
+    _json_decoder = json.JSONDecoder()
+
+    def __init__(self, defaults=None, *,
+                 dict_type=dict, default_section='DEFAULTS'):
+        self._dict = dict_type
+        self._default_section = default_section
+        self._sections = self._dict()
+        self._defaults = self._dict()
+        self._proxies = self._dict()
+        self._proxies[default_section] = SectionProxy(self, default_section)
+        if defaults:
+            self.read_dict({default_section: defaults})
+
+    def sections(self):
+        """Return a list of section names, excluding [DEFAULT]"""
+        return list(self._sections.keys())
+
+    def add_section(self, section):
+        """Create a new section in the configuration.
+
+        Raise DuplicateSectionError if a section by the specified name
+        already exists. Raise ValueError if name is DEFAULT.
+        """
+        if (section == self.default_section) or \
+           (not self._section_re.match(section)):
+            raise ValueError('Invalid section name: %r' % section)
+
+        if section in self._sections:
+            raise DuplicateSectionError(section)
+        self._sections[section] = self._dict()
+        self._proxies[section] = SectionProxy(self, section)
+
+    def remove_section(self, section):
+        """Delete a section.
+
+        Returns True if the section existed previously, False otherwise.
+        """
+        existed = section in self._sections
+        if existed:
+            del self._sections[section]
+            del self._proxies[section]
+        return existed
+
+    def __getitem__(self, key):
+        if key != self.default_section and not self.has_section(key):
+            raise KeyError(key)
+        return self._proxies[key]
+
+    def __setitem__(self, key, value):
+        # To conform with the mapping protocol, overwrites existing values in
+        # the section.
+        if key == self.default_section:
+            self._defaults.clear()
+        elif key in self._sections:
+            self._sections[key].clear()
+        self.read_dict({key: value})
+
+    def __delitem__(self, key):
+        if key == self.default_section:
+            raise ValueError("Cannot remove the default section.")
+        if not self.remove_section(key):
+            raise KeyError(key)
+
+    def __contains__(self, key):
+        return key == self.default_section or key in self.sections
+
+    def __len__(self):
+        return len(self._sections) + 1
+
+    def __iter__(self):
+        return itertools.chain((self.default_section,), self._sections.keys())
+
+    def options(self, section):
+        """Return a list of option names for the given section name."""
+        try:
+            opts = list[set(self._sections[section].keys() +
+                            self._defaults.keys())]
+        except KeyError:
+            raise NoSectionError(section)
+        return opts
+
+    def get(self, section, option, fallback=_UNSET, *, vars=None):
+        """Get an option value for a given section.
+
+        If `vars' is provided, it must be a dictionary. The option is looked up
+        in `vars' (if provided), `section', and in `DEFAULTSECT' in that order.
+        If the key is not found and `fallback' is provided, it is used as
+        a fallback value. `None' can be provided as a `fallback' value.
+
+        The section DEFAULT is special.
+        """
+        try:
+            d = self._unify_values(section, vars)
+        except NoSectionError:
+            if fallback is _UNSET:
+                raise
+            else:
+                return fallback
+        option = self.optionxform(option)
+        try:
+            value = d[option]
+        except KeyError:
+            if fallback is _UNSET:
+                raise NoOptionError(option, section)
+            else:
+                return fallback
+        return value
+
+    def set(self, section, option, value=None):
+        if not section or section == self.default_section:
+            sectdict = self.defaults
+        else:
+            try:
+                sectdict = self._sections[section]
+            except KeyError:
+                raise NoSectionError(section)
+            sectdict[option] = value
+
+    def read(self, filnames, encoding=None):
+        pass
+
+    def read_file(self, fp, filename=None):
+        pass
+
+    def read_dict(self, dictionary):
+        pass
+
+    def read_string(self, string, fpname=None):
+        sections_added = set()
+        entries_added = set()
+
+        sectname = None
+        cursect = None
+
+        idx = 0
+        lineno = 0
+
+        while True:
+            mo = self._header_re.match(string, idx)
+            if mo:
+                sectname = mo.group('section')
+
+                # check that section has not occured in this file before
+                if sectname in sections_added:
+                    raise DuplicateSectionError(sectname)
+                sections_added.add(sectname)
+                entries_added = set()
+
+                # find or create the section
+                if sectname == self.default_section:
+                    cursect = self._defaults
+                elif sectname in self._sections:
+                    cursect = self._sections[sectname]
+                else:
+                    cursect = self._dict()
+                    self._sections[sectname] = cursect
+                    self._proxies[sectname] = SectionProxy(self, sectname)
+
+                idx = mo.end() + 1
+            else:
+                # consume blank lines and comments
+                mo = self._blank_re.match(string, idx)
+                if mo:
+                    idx = mo.end()
+                    print("line")
+                else:
+                    mo = self._key_re.match(string, idx)
+                    if not mo:
+                        raise ParseError(
+                            "expected section, comment or newline")
+
+                    # read key
+                    optname = mo.group('key')
+                    idx = mo.end()
+                    if optname in entries_added:
+                        raise DuplicateOptionError(sectname, optname,
+                                                   fpname, lineno)
+                    entries_added.add(optname)
+
+                    # read value
+                    # TODO increment lineno
+                    optval, idx = self._json_decoder.raw_decode(string, idx)
+                    cursect[optname] = optval
+
+                    # consume remaining comments and whitespace
+                    mo = self._eol_re.match(string, idx)
+                    if not mo:
+                        raise ParseError(repr(string[idx:]))
+                    idx = mo.end()
+            lineno += 1
+
+    @property
+    def default_section(self):
+        # default section should be read-only
+        return self._default_section
+
+
+class SectionProxy(MutableMapping):
+    """A proxy for a single section from a parser."""
+
+    def __init__(self, parser, name):
+        """Creates a view on a section of the specified `name` in `parser`."""
+        self._parser = parser
+        self._name = name
+
+    def __repr__(self):
+        return '<Section: {}>'.format(self._name)
+
+    def __getitem__(self, key):
+        if not self._parser.has_option(self._name, key):
+            raise KeyError(key)
+        return self._parser.get(self._name, key)
+
+    def __setitem__(self, key, value):
+        self._parser._validate_value_types(option=key, value=value)
+        return self._parser.set(self._name, key, value)
+
+    def __delitem__(self, key):
+        if not (self._parser.has_option(self._name, key) and
+                self._parser.remove_option(self._name, key)):
+            raise KeyError(key)
+
+    def __contains__(self, key):
+        return self._parser.has_option(self._name, key)
+
+    def __len__(self):
+        return len(self._options())
+
+    def __iter__(self):
+        return self._options().__iter__()
+
+    def _options(self):
+        if self._name != self._parser.default_section:
+            return self._parser.options(self._name)
+        else:
+            return self._parser.defaults()
+
+    def get(self, option, fallback=_UNSET, *, vars=None):
+        return self._parser.get(self._name, option, vars=vars,
+                                fallback=fallback)
+
+    @property
+    def parser(self):
+        # The parser object of the proxy is read-only.
+        return self._parser
+
+    @property
+    def name(self):
+        # The name of the section on a proxy is read-only.
+        return self._name
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Command
+
+setup(name='JSONConfigParser',
+      version='0.0.1',
+      author='Ben Mather',
+      author_email='contact@bwhmather.com',
+      url='https://github.com/bwhmather/json-config-parser/',
+      packages=['jsonconfigparser'])
+
