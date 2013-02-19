85ed15666aa96e942ddeefb3187b3f934f723328
Ben Mather <bwhmather@gmail.com>
2013-02-19 17:21:35 +0000

    Flatten project heirarchy

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Command
+from distutils.core import setup
 
 setup(name='JSONConfigParser',
       version='0.0.1',
       author='Ben Mather',
       author_email='contact@bwhmather.com',
       url='https://github.com/bwhmather/json-config-parser/',
-      packages=['jsonconfigparser'])
-
+      py_modules=['jsonconfigparser'])
