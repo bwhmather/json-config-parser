6fd397f30e644df4de383d97f9e2bf8a7c9c402a
Ben Mather <bwhmather@bwhmather.com>
2014-04-11 18:03:01 +0100

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
-#!/usr/bin/env python
+"""
+JSON Config Parser
+==================
 
-from distutils.core import setup
+A straightforward and unambiguous config file parser.
+Parses ini style files with json keys.
+"""
+from setuptools import setup
 
-setup(name='JSONConfigParser',
-      version='0.0.1',
-      author='Ben Mather',
-      author_email='contact@bwhmather.com',
-      url='https://github.com/bwhmather/json-config-parser/',
-      py_modules=['jsonconfigparser'])
+setup(
+    name='json-config-parser',
+    version='0.0.1',
+    author='Ben Mather',
+    author_email='bwhmather@bwhmather.com',
+    description='A straightforward and unambiguous config file parser.',
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    platforms='any',
+    url='https://github.com/bwhmather/json-config-parser/',
+    py_modules=['jsonconfigparser'],
+    test_suite='test_jsonconfigparser.suite',
+    )
